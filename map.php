8fa9a67b96030c4b5f12e9aed7fada18b10f12a8
Sang <sang.doanbk1@hcmut.edu.vn>
2025-04-22 18:49:19 +0700

    delete key

--- map.php
+++ map.php
@@ -12,8 +12,6 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     <!-- font awesome -->
     <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.2/css/all.min.css" rel="stylesheet">
-
-    <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyAfqhxj764ggh1HKjie4AwkPmGoijwnkiw"></script>
     
     <link rel="stylesheet" href="https://unpkg.com/leaflet/dist/leaflet.css" />
     <script src="https://unpkg.com/leaflet/dist/leaflet.js"></script>
